6a418c176147e8f2d8f51f081ec2d104fb7e52ea
Christopher Scott <cscott319@gatech.edu>
2024-09-26 23:02:24 -0400

    Implemented producer and consumer thread

--- pthreads/src/ProducerConsumer.c
+++ pthreads/src/ProducerConsumer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 #define BUF_SIZE 3 /*size of shared buffer*/
 int buffer[BUF_SIZE]; /*shared buffer*/
@@ -14,8 +15,54 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; /*mutex lock for buffer*/
 pthread_cond_t c_cons = PTHREAD_COND_INITIALIZER; /*consumer waits on cv*/
 pthread_cond_t c_prod = PTHREAD_COND_INITIALIZER; /*producer waits on cv*/
 
-void* producer (void* param);
-void* consumer (void* param);
+void* producer (void* param)
+{
+    int i;
+    for(i = 1;i <= 20; i++){
+        pthread_mutex_lock(&mutex);
+        if(num > BUF_SIZE){
+            exit(EXIT_FAILURE);/*Overflow*/
+        }
+
+        while(num == BUF_SIZE){
+            /*Blocks if buffer is full*/
+            pthread_cond_wait(&c_prod,&mutex);
+        }
+        buffer[add] = i; /*Buffer is not full, add element*/
+        add = (add+1) % BUF_SIZE;
+        num++;
+        pthread_mutex_unlock(&mutex);
+
+        pthread_cond_signal(&c_cons);
+        printf("producer inserted %d\n", i); fflush (stdout);
+    } 
+    printf("producer exiting\n"); fflush(stdout);
+    return 0;
+}
+void* consumer (void* param){
+    int i;
+
+    while(true)
+    {
+        pthread_mutex_lock(&mutex);
+        if(num < 0){
+            /*Underflow*/
+            exit(EXIT_FAILURE);
+        }
+        while(num == 0){
+            /*Block if buffer is empty*/
+            pthread_cond_wait(&c_cons, &mutex);
+        }
+        i = buffer[rem];
+        rem = (rem+1) % BUF_SIZE;
+        num--;
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&c_prod);
+        printf("Consume value %d\n",i); fflush(stdout);
+        
+    }
+   // printf("consumer exiting\n"); fflush(stdout);
+}
 
 int main(int argc, char *argv [])
 {
@@ -34,5 +81,9 @@ int main(int argc, char *argv [])
         exit(EXIT_FAILURE);
     }
 
+    pthread_join(tid1,NULL);/*Wait for producer to exit*/
+    pthread_join(tid2,NULL);/*Wait for consumer to exit*/
+
     return 0;
 }
+
